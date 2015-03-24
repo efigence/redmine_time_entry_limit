e01534102adc19e53c2e104ee6d512b924fb06da
Rafał Lisowski <korin@users.noreply.github.com>
2015-03-24 15:57:21 +0100

    prevent destroy protected time reports

--- lib/redmine_timesheet_plugin/patches/time_entry_patch.rb
+++ lib/redmine_timesheet_plugin/patches/time_entry_patch.rb
@@ -9,6 +9,7 @@ module RedmineTimesheetPlugin
           validate :validate_time_limit
           validate :validate_day_back_limit
           validate :validate_issue_id
+          before_destroy :validate_day_back_limit
         end
       end
 
@@ -38,6 +39,7 @@ module RedmineTimesheetPlugin
           if !User.current.admin? && spent_on
             limit = Setting.plugin_redmine_time_entry_limit['day_limit'].to_i || 7
             errors.add :spent_on, :too_many_days, limit: limit if spent_on < Date.today - limit.days
+            return false
           end
         end
       end
